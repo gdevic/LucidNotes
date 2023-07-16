784ef3d01c5acc04f0bc50d564bc13692c3912b5
Goran Devic <gdevic@yahoo.com>
2023-07-16 12:26:45 -0500

    Load a document

--- src/WidgetTextEdit.cpp
+++ src/WidgetTextEdit.cpp
@@ -1,5 +1,9 @@
 #include "WidgetTextEdit.h"
 #include "ui_WidgetTextEdit.h"
+#include <QFileDialog>
+#include <QMimeData>
+#include <QMimeDatabase>
+#include <QStringDecoder>
 
 WidgetTextEdit::WidgetTextEdit(QWidget *parent) :
     QWidget(parent),
@@ -7,8 +11,39 @@ WidgetTextEdit::WidgetTextEdit(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    // Testing: show or hide editing toolbar
-    connect(ui->pushButton, &QPushButton::clicked, this, [=](bool shown) { setEditingToolbar(shown); });
+    // Testing:
+    //   * Show or hide editing toolbar
+    //   * Load a document file to be edited
+    connect(ui->pbShowHide, &QPushButton::clicked, this, [=](bool shown) { showToolbar(shown); });
+    connect(ui->pbLoad, &QPushButton::clicked, this, [=]() {
+        QFileDialog fileDialog(this, tr("Open Document..."));
+        fileDialog.setAcceptMode(QFileDialog::AcceptOpen);
+        fileDialog.setFileMode(QFileDialog::ExistingFile);
+        fileDialog.setMimeTypeFilters({"text/html", "text/markdown", "text/plain"});
+        if (fileDialog.exec() != QDialog::Accepted)
+            return;
+        const QString f = fileDialog.selectedFiles().constFirst();
+        if (!QFile::exists(f))
+            return;
+        QFile file(f);
+        if (!file.open(QFile::ReadOnly))
+            return;
+        QByteArray data = file.readAll();
+        QMimeDatabase db;
+        const QString &mimeTypeName = db.mimeTypeForFileNameAndData(f, data).name();
+        if (mimeTypeName == u"text/html")
+        {
+            auto encoding = QStringDecoder::encodingForHtml(data);
+            QString str = QStringDecoder(encoding ? *encoding : QStringDecoder::Utf8)(data);
+            QUrl fileUrl = f.startsWith(u':') ? QUrl(f) : QUrl::fromLocalFile(f);
+            ui->textEdit->document()->setBaseUrl(fileUrl.adjusted(QUrl::RemoveFilename));
+            ui->textEdit->setHtml(str);
+        }
+        else if (mimeTypeName == u"text/markdown")
+            ui->textEdit->setMarkdown(QString::fromUtf8(data));
+        else
+            ui->textEdit->setPlainText(QString::fromUtf8(data));
+    });
 }
 
 WidgetTextEdit::~WidgetTextEdit()
@@ -16,8 +51,24 @@ WidgetTextEdit::~WidgetTextEdit()
     delete ui;
 }
 
-void WidgetTextEdit::setEditingToolbar(bool shown)
+bool WidgetTextEdit::isToolbarVisible()
 {
-    ui->widget->setVisible(shown);
+    return ui->editingToolbar->isVisible();
+}
+
+void WidgetTextEdit::showToolbar(bool shown)
+{
+    ui->editingToolbar->setVisible(shown);
     emit editingToolbarChanged(shown);
+}
+
+QTextDocument *WidgetTextEdit::getDoc()
+{
+    return ui->textEdit->document();
+}
+
+void WidgetTextEdit::setDoc(QTextDocument *doc)
+{
+    ui->textEdit->setDocument(doc);
+    emit docChanged(doc);
 }
--- src/WidgetTextEdit.h
+++ src/WidgetTextEdit.h
@@ -2,6 +2,7 @@
 #define WIDGETTEXTEDIT_H
 
 #include <QWidget>
+#include <QTextDocument>
 
 namespace Ui {
 class WidgetTextEdit;
@@ -15,17 +16,21 @@ public:
     explicit WidgetTextEdit(QWidget *parent = nullptr);
     ~WidgetTextEdit();
 
-    Q_PROPERTY(bool editingToolbar MEMBER editingToolbar WRITE setEditingToolbar NOTIFY editingToolbarChanged);
+    Q_PROPERTY(bool editingToolbar READ isToolbarVisible WRITE showToolbar NOTIFY editingToolbarChanged);
+    Q_PROPERTY(QTextDocument* doc READ getDoc WRITE setDoc NOTIFY docChanged);
 
 public slots:
-    void setEditingToolbar(bool shown);
+    bool isToolbarVisible();
+    void showToolbar(bool shown);
+    QTextDocument *getDoc();
+    void setDoc(QTextDocument *doc);
 
 signals:
     void editingToolbarChanged(bool);
+    void docChanged(QTextDocument*);
 
 private:
     Ui::WidgetTextEdit *ui;
-    bool editingToolbar; // Show or hide editing toolbar
 };
 
 #endif // WIDGETTEXTEDIT_H
